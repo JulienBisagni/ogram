5c75749276913043eab2e8d950ded0b384234e33
Yanan Dherissart <185847679@qq.com>
2020-02-05 17:47:15 +0100

    add swipe down + basic left and right

--- app/javascript/plugins/swipePost.js
+++ app/javascript/plugins/swipePost.js
@@ -9,14 +9,24 @@ const swipePost = () => {
       var postHeight = $(this).outerHeight();
       var postIndex = $(this).index() +1;
       var marginHeight = postHeight * postIndex;
-      var nMarginHeight = -(postHeight * postIndex);
+      var downIndex = $(this).index() -1 ;
+      var downHeight = postHeight * downIndex;
       console.log(marginHeight);
       $(this).swipe( {
         //Generic swipe handler for all directions
         swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
           if (direction == "up") {
             $(".test-swipe > div:first-child").css("margin-top", `-${marginHeight}px`);
-          }
+          };
+            if (direction == "down") {
+            $(".test-swipe > div:first-child").css("margin-top", `-${downHeight}px`);
+          };
+          if (direction == "left") {
+            $(".test-swipe > div:first-child").css("margin-top", `-${marginHeight}px`);
+          };
+          if (direction == "right") {
+            $(".test-swipe > div:first-child").css("margin-top", `-${marginHeight}px`);
+          };
         },
         //Default is 75px, set to 0 for demo so any distance triggers swipe
          threshold:0
