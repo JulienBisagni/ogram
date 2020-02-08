4ca7b28cc423a49005603c353abf7acc62e1be47
Yanan Dherissart <185847679@qq.com>
2020-02-08 16:00:17 +0100

    add comment with ajaxified create+delete method

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -20,12 +20,15 @@ class ContentsController < ApplicationController
         infoWindow: render_to_string(partial: "info_window", locals: { content: content })
       }
     end
-
   end
 
   def show
     @content = Content.find(params[:id])
     authorize @content
+    @comment = Comment.new
+    @comments = @content.comments.select do |comment|
+      comment.persisted?
+    end
   end
 
   def create
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :content
+  validates :content, presence: true
 end
