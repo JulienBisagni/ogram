2519460eaf9a9498634594f9d9292a74c6888314
Julien Bisagni <julienbisagni@gmail.com>
2020-02-13 00:34:08 +0100

    Added seed to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 content_list = [
-  [ User.first, "1" ],
-  [ User.first, "2" ],
-  [ User.first, "3" ],
-  [ User.first, "4" ]
+  [
+    User.first,
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "app/assets/images/contents/photo1.jpg"
+  ],
+  [
+    User.first,
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "app/assets/images/contents/photo2.jpg"
+  ],
+  [
+    User.first,
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "app/assets/images/contents/photo3.jpg"
+  ],
+
 ]
 
-content_list.each do |user, description |
-  Content.create( user: user, description: description,  )
+content_list.each do |user, description, file |
+  content = Content.new( user: user, description: description)
+  media = File.open(file)
+  content.photo.attach(io: media, filename:'nes.png', content_type: 'image/jpg')
+  content.save!
 end
