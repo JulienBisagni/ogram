590f1cbdcf9f4dc91207861bc17528961afb9979
Julien Bisagni <julienbisagni@gmail.com>
2020-02-25 20:34:52 +0100

    Ratio to hide content on all app

--- app/models/content.rb
+++ app/models/content.rb
@@ -9,6 +9,13 @@ class Content < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_place?
 
   def downvoted?(user)
+    number_of_user_views = self.user_views.count #20
+    number_of_user_views_downvoted = self.user_views.where(downvoted: true).count #10
+    if number_of_user_views > 0
+      ratio = number_of_user_views_downvoted / number_of_user_views
+      return false if number_of_user_views >= 2 && ratio >= 0.5
+    end
+
     user.user_views.select(&:downvoted).map(&:content).include?(self)
   end
 
